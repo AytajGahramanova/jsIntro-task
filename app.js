cd1ae48553b20fd3fe7f27db0444e7a84f3c8af7
aytac <qehremanova.aytac2003@gmail.com>
2023-10-12 01:20:24 +0400

    task is added

---
+++ app.js
@@ -0,0 +1,42 @@
+// TASK - 1 //
+// let a = 234;
+// let b = 234 % 10;
+// let c = 23 / 10;
+// console.log(b);
+// console.log(parseInt(c));
+// console.log(parseInt(c) , b);
+
+// // TASK - 3 //
+// let n = 4;
+// let m = 8;
+// let p = ((n * 2) + (m * 2));
+// console.log(`Dördbucaqlının perimetri: ${p}`);
+
+// // TASK - 4 //
+// let a = 15;
+// let b = 2;
+// console.log("a və b ədələrinin hasili:" , a * b);
+
+// TASK - 5 //
+// let a = 10;
+// let n = 3;
+// console.log(a**n);
+
+// TASK - 6 //
+// let a = 992;
+// let n = 10;
+// let c = a/n;
+// console.log(`Ədəd: ${a}`);
+// console.log("Ədədin axırıncı rəqəmi silindikdən sonra:" , parseInt(c) );
+
+// TASK - 7 //
+// let n = 95;
+// console.log(n % 10);
+
+// //TASK - 8 //
+// let x = 4567;
+// let y = (4567%10)*1000;
+// let z = (parseInt(x/10)%10)*100;
+// let q = (parseInt(x/100)%10)*10;
+// let f = parseInt(x/1000);
+// console.log(y + z + q + f);
